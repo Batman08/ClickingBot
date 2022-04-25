fdd8df9af95b85d237ba4dbc1430106a96c5b2e5
bilal asghar <asgharbilal8@gmail.com>
2022-04-25 01:58:04 +0100

    created clicker function to perform a click

--- ClickingBot/ClickingBot.py
+++ ClickingBot/ClickingBot.py
@@ -1,1 +1,16 @@
+import time 
+import threading
+from pynput.mouse import Controller, Button
+from pynput.keyboard import Listener, KeyCode
 
+TOGGLE_KEY = KeyCode(char="t")
+clicking = False
+mouse = Controller()
+
+def clicker():
+    #perform a click if clicking variable is set to true
+    while True:
+        if clicking:
+            mouse.click(Button.left,1)
+        #sleep so theres enough time to activate or deactivate
+        time.sleep(0.001)
